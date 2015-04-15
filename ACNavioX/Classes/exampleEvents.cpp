d0a5220a23075e9955f7ef88861ac11ad2717209
Raul Teixeira <raul.teixeira@camara.leg.br>
2015-04-15 18:57:13 -0300

    Example files. Not being used.

---
+++ ACNavioX/Classes/exampleEvents.cpp
@@ -0,0 +1,53 @@
+ //Create a "one by one" touch event listener (processes one touch at a time)
+    auto listener1 = EventListenerTouchOneByOne::create();
+    // When "swallow touches" is true, then returning 'true' from the onTouchBegan method will "swallow" the touch event, preventing other listeners from using it.
+    listener1->setSwallowTouches(true);
+
+    // Example of using a lambda expression to implement onTouchBegan event callback function
+    listener1->onTouchBegan = [](Touch* touch, Event* event){
+        // event->getCurrentTarget() returns the *listener's* sceneGraphPriority node.
+        auto target = static_cast<Sprite*>(event->getCurrentTarget());
+
+        //Get the position of the current point relative to the button
+        Point locationInNode = target->convertToNodeSpace(touch->getLocation());
+        Size s = target->getContentSize();
+        Rect rect = Rect(0, 0, s.width, s.height);
+
+        //Check the click area
+        if (rect.containsPoint(locationInNode))
+        {
+            log("sprite began... x = %f, y = %f", locationInNode.x, locationInNode.y);
+            target->setOpacity(180);
+            return true;
+        }
+        return false;
+    };
+
+    //Trigger when moving touch
+    listener1->onTouchMoved = [](Touch* touch, Event* event){
+        auto target = static_cast<Sprite*>(event->getCurrentTarget());
+        //Move the position of current button sprite
+        target->setPosition(target->getPosition() + touch->getDelta());
+    };
+
+    //Process the touch end event
+    listener1->onTouchEnded = [=](Touch* touch, Event* event){
+        auto target = static_cast<Sprite*>(event->getCurrentTarget());
+        log("sprite onTouchesEnded.. ");
+        target->setOpacity(255);
+        //Reset zOrder and the display sequence will change
+        if (target == sprite2)
+        {
+            sprite1->setZOrder(100);
+        }
+        else if(target == sprite1)
+        {
+            sprite1->setZOrder(0);
+        }
+    };
+
+
+    //Add listener
+    _eventDispatcher->addEventListenerWithSceneGraphPriority(listener1, sprite1);
+    _eventDispatcher->addEventListenerWithSceneGraphPriority(listener1->clone(), sprite2);
+    _eventDispatcher->addEventListenerWithSceneGraphPriority(listener1->clone(), sprite3);
